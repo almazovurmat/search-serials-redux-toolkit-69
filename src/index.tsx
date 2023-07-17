84944593fbfa4961540760c05679e471b628876a
Almazov Urmat <almazov.urmat@gmail.com>
2023-07-17 22:33:12 +0600

    import bootstrap to the project

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {store} from "./app/store";
 
 const root = ReactDOM.createRoot(
